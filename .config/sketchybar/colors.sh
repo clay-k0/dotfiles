2b54a8038a44a5a9b805e75af309f23a86a4a4df
Clayton King <ClaytonKing03@gmail.com>
2024-03-24 03:36:36 -0400

    updates

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -16,6 +16,6 @@ export GREEN=0xff72966c
 export YELLOW=0xff72966c
 export RED=0xffb85651
 
-export BAR_COLOR=0xff252424
-export ITEM_BG_COLOR=0xff343334
+export BAR_COLOR=0xff1e1d1d
+export ITEM_BG_COLOR=0xff2d2c2d
 export ACCENT_COLOR=$WHITE
