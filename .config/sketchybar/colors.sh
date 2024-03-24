e98afa56ef08c6cff7189b6d9ecb165b59556f1e
Clayton King <ClaytonKing03@gmail.com>
2024-03-24 00:14:31 -0400

    copying updated configs over before stow

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -11,11 +11,11 @@
 # export ACCENT_COLOR=0xffa89983
 
 # Gruvbox
-export WHITE=0xff928273
+export WHITE=0xffa89883
 export GREEN=0xff72966c
 export YELLOW=0xff72966c
 export RED=0xffb85651
 
-export BAR_COLOR=0xff282727
+export BAR_COLOR=0xff242322
 export ITEM_BG_COLOR=0xff343334
-export ACCENT_COLOR=0xff928273
+export ACCENT_COLOR=$WHITE
