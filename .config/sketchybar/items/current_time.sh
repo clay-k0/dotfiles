60d6b6c4c7389fa47c81ff7acb709c3f9c0a6790
Clayton King <ClaytonKing03@gmail.com>
2024-03-29 04:54:36 -0400

    updates

--- .config/sketchybar/items/current_time.sh
+++ .config/sketchybar/items/current_time.sh
@@ -3,5 +3,5 @@
 sketchybar --add item current_time right \
 	--set current_time icon="time" \
 	icon.color=$ACCENT_COLOR \
-	update_freq=43200 \
+	update_freq=30 \
 	script="$PLUGIN_DIR/current_time.sh"
--- .config/sketchybar/items/date.sh
+++ .config/sketchybar/items/date.sh
@@ -3,4 +3,5 @@
 sketchybar --add item date right \
 	--set date icon="date" \
 	icon.color=$ACCENT_COLOR \
+	update_freq=43200 \
 	script="$PLUGIN_DIR/date.sh"
