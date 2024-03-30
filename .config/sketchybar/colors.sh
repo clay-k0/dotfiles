12a182f2acf62af94ec3e4a38cd34cacf6b2b56b
Clayton King <ClaytonKing03@gmail.com>
2024-03-29 22:19:09 -0400

    updates

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,11 +1,20 @@
 #!/bin/zsh
 
 # Gruvbox
-export WHITE=0xff928273
-export GREEN=0xff72966c
-export YELLOW=0xffd69821
-export RED=0xffb85651
+# export WHITE=0xff928273
+# export GREEN=0xff72966c
+# export YELLOW=0xffd69821
+# export RED=0xffb85651
+#
+# export BAR_COLOR=0xff1d1c1c
+# export ITEM_BG_COLOR=0xff3c3735
+# export ACCENT_COLOR=0xffebdab1
 
-export BAR_COLOR=0xff1d1c1c
-export ITEM_BG_COLOR=0xff3c3735
-export ACCENT_COLOR=0xffebdab1
+# Gruvbox (Hard)
+export WHITE=0xff928273
+export GREEN=0xff689c69
+export YELLOW=0xffd69821
+export RED=0xffcb231d
+
+export BAR_COLOR=0xff141617
+export ACCENT_COLOR=0xffa89883
